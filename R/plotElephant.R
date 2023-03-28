418cb1fd16c126a54c4b095e1dd93a17335c461b
Kline DuBose <kline.dubose@hotmail.com>
2023-03-28 12:17:08 -0600

    updated plot and summary s3 objects to be inline with correct r documentation

--- R/plotElephant.R
+++ R/plotElephant.R
@@ -2,24 +2,24 @@
 #'
 #' A general function to plot the results of a White Elephant simulation.
 #'
-#' @param sim An object of class `elphList` or `elphSum` to be summarized
+#' @param x An object of class `elphList` or `elphSum` to be summarized
 #' @param ... Additional arguments in `plot` function
 #'
 #' @return A plot showing the distribution of each winning seat and how often they won.
 #'
 #' @export plot.partyGames
 #' @export
-plot.partyGames <- function(sim, ...){
-  if(inherits(sim, "elphList") | inherits(sim, "elphSum")){
+plot.partyGames <- function(x, ...){
+  if(inherits(x, "elphList") | inherits(x, "elphSum")){
 
-    if(inherits(sim, "elphList")){
-      sim <- summary.partyGames(sim)
+    if(inherits(x, "elphList")){
+      x <- summary.partyGames(x)
     }
 
   graphics::barplot(
-    sim[[1]] / sum(sim[[1]]),
-    ylim = c(0, max(sim[[1]] / sum(sim[[1]])) + 0.05),
-    sub = sprintf("Distribution of %i simulated white elephant games", sum(sim[[1]])),
+    x[[1]] / sum(x[[1]]),
+    ylim = c(0, max(x[[1]] / sum(x[[1]])) + 0.05),
+    sub = sprintf("Distribution of %i simulated white elephant games", sum(x[[1]])),
     main = "Who gets the present?",
     col = "cornflowerblue",
     border = "dimgray"
--- R/summaryElephant.R
+++ R/summaryElephant.R
@@ -2,22 +2,22 @@
 #'
 #' A general function to summarize a simulation of white elephant games.
 #'
-#' @param sim An object of class `elphList` to be summarized
-#' @param ... Additional arguments
+#' @param object An object of class `elphList` to be summarized
+#' @param ... Additional arguments affecting the summary produced
 #'
 #' @return An object of class `elphSum` containing a table of moves over a
 #' simulation and a table of times each chair won.
 #' @export summary.partyGames
 #' @export
 #'
-summary.partyGames <- function(sim, ...){
-  if (inherits(sim, "elphList")){
+summary.partyGames <- function(object, ...){
+  if (inherits(object, "elphList")){
 
-  moves <- sapply(sim, "[[", "moves")
+  moves <- sapply(object, "[[", "moves")
 
   moves <- table(moves)
 
-  winner <- sapply(sim, "[[", "winner")
+  winner <- sapply(object, "[[", "winner")
 
   winner <- table(winner)
 
