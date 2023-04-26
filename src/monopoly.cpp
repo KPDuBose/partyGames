7f9a2d0fea12d337485be9e7506dea3e11ed47d3
Kline DuBose <kline.dubose@hotmail.com>
2023-04-26 13:28:07 -0600

    Added functions to draw Community Chest and Chance cards. Refined go-to-jail functions.

--- src/monopoly.cpp
+++ src/monopoly.cpp
@@ -21,13 +21,127 @@ using namespace Rcpp;
 //'@export
 IntegerVector diceRoll(int sides, int numDice);
 
-IntegerVector chanceCards(int numCards){
-
+IntegerVector cards(int numCards){
   //Shuffle the deck
   IntegerVector chanceDeck = sample(numCards, numCards, false);
 
   return chanceDeck;
+}
 
+IntegerVector communityDraw(
+  int token,
+  int card,
+  int count
+){
+  IntegerVector result;
+
+  //Cards that change position on the board
+  //Move to Go
+  if (card == 1){
+    return result = {1, count, 0};
+  }
+  //Go to Jail
+  if (card == 6){
+    count += 2;
+    return result = {11, count, 1};
+  }
+
+  //Cards that don't change position on the board
+  return result = {token, count, 0};
+}
+
+IntegerVector chanceDraw(
+  int token,
+  int card,
+  int count
+){
+  IntegerVector result = {token, count, 0};
+
+  //Set locations of the utility spaces and railroad spaces
+  IntegerVector utility = {13,29};
+  IntegerVector railroads = {6, 16, 26, 36};
+  // IntegerVector closeRail;
+  // IntegerVector closeUtil;
+
+  //Cards that change position
+
+  //Boardwalk
+  if (card == 1){
+    result[0] = 40;
+    return result;
+  }
+
+  //Go
+  if (card == 2){
+    result[0] = 1;
+    return result;
+  }
+
+  //Illinois Ave
+  if (card == 3){
+    result[0] = 25;
+    return result;
+  }
+
+  //St. Charles
+  if (card == 4){
+    result[0] = 12;
+    return result;
+  }
+
+  //Nearest Railroads
+  if (card == 5 || card == 6){
+    //Based on position of chance spaces and position of RailRoads
+    if (token == 8){
+      result[0] = 16;
+      return result;
+    }
+
+    if (token == 23){
+      result[0] = 26;
+      return result;
+    }
+
+    if (token == 37){
+      result[0] = 6;
+      return result;
+    }
+  }
+
+  //Utilities
+  if (card == 7){
+    if (token == 8 || token == 37){
+      result[0] = utility[0];
+      return result;
+    }
+    else {
+      result[0] = utility[1];
+      return result;
+    }
+  }
+
+  //Go back three spaces
+  if (card == 10){
+    result[0] = token - 3;
+    return result;
+  }
+
+  //Go to jail
+  if (card == 11){
+    result[0] = 11;
+    result[1] = count + 2;
+    result[2] = 1;
+    return result;
+  }
+
+  //Go to Reading Railroad
+  if (card == 14){
+    result[0] = 6;
+    return result;
+  }
+
+  //Cards that don't change position
+  return result = {token, count, 0};
 }
 
 // [[Rcpp::export]]
@@ -37,33 +151,44 @@ List monopoly(
   int numDice = 2
 ){
   //Initialize and shuffle the Chance and Community decks
-  IntegerVector chance = chanceCards(16);
-  IntegerVector community = chanceCards(16);
+  IntegerVector chance = cards(16);
+  IntegerVector community = cards(16);
+  int topChance = 0;
+  int topCommunity = 0;
+  IntegerVector communityRes;
+  IntegerVector chanceRes;
 
-  //Set locations of the utility spaces and railroad spaces
-  IntegerVector utility = {13,29};
-  IntegerVector railroads = {6, 16, 26, 36};
+  //Track Community Chest and Chance card draws
+  IntegerVector chanceTrack;
+  IntegerVector communityTrack;
 
   //Set up spaces tracker vector
   IntegerVector spaceTracker;
 
   //Turn counter
   int count = 0;
+  IntegerVector countTrack;
 
   //Double tracker
   int dTrack = 0;
+  IntegerVector dTracker;
 
   //Token movement
   int token = 1;
 
-  //Set up turn movements
+  //Dice vector set up
+  IntegerVector dice;
+  int roll;
+
+  //Set up turn
   while (count < maxTurns){
     count += 1;
     dTrack = 0;
+    countTrack.push_back(count);
 
     //Roll the dice
-    IntegerVector dice = diceRoll(sides, numDice);
-    int roll = sum(dice);
+    dice = diceRoll(sides, numDice);
+    roll = sum(dice);
 
     token += roll;
 
@@ -74,11 +199,66 @@ List monopoly(
 
     //Append to spaceTracker
     spaceTracker.push_back(token);
+    dTracker.push_back(dTrack);
+
+    //Draw a Community Chest card
+    if (token == 3 || token == 18 || token == 34){
+      if (topCommunity == 16){
+        topCommunity -= 16;
+      }
+
+      communityTrack.push_back(community[topCommunity]);
+
+      communityRes = communityDraw(
+        token,
+        community[topCommunity],
+        count);
+
+      token = communityRes[0];
+      count = communityRes[1];
+
+      topCommunity += 1;
+
+      if (communityRes[2] == 1){
+        //Append to spaceTracker
+        spaceTracker.push_back(token);
+        continue;
+      }
+    }
+
+    //Draw a Chance card
+    if (token == 8 || token == 23 || token == 37){
+      if (topChance == 16){
+        topChance -= 16;
+      }
+
+      chanceTrack.push_back(chance[topChance]);
+
+      chanceRes = chanceDraw(
+        token,
+        chance[topChance],
+        count
+      );
+
+      token = chanceRes[0];
+      count = chanceRes[1];
+
+      topChance += 1;
+
+      if (chanceRes[2] == 1){
+        //Append to spaceTracker
+        spaceTracker.push_back(token);
+        continue;
+      }
+    }
 
     //Check if rolled doubles
-    while(dice[0] == dice[1]){
+    while (dice[0] == dice[1] && dTrack != 3){
       dTrack += 1;
-      if (dTrack > 2){
+      dTracker.push_back(dTrack);
+
+      //Check if third double rolled
+      if (dTrack == 3){
         token = 11;
         spaceTracker.push_back(token);
         count += 2; //Misses turns due to being in jail
@@ -86,8 +266,8 @@ List monopoly(
       }
 
       //Roll the dice
-      IntegerVector dice = diceRoll(sides, numDice);
-      int roll = sum(dice);
+      dice = diceRoll(sides, numDice);
+      roll = sum(dice);
 
       token += roll;
 
@@ -98,16 +278,71 @@ List monopoly(
 
       //Append to spaceTracker
       spaceTracker.push_back(token);
+
+      //Draw a Community Chest card (For Doubles)
+      if (token == 3 || token == 18 || token == 34){
+        if (topCommunity == 14){
+          topCommunity -= 14;
+        }
+
+        communityTrack.push_back(community[topCommunity]);
+
+        communityRes = communityDraw(
+          token,
+          community[topCommunity],
+                   count);
+
+        token = communityRes[0];
+        count = communityRes[1];
+
+        topCommunity += 1;
+
+        if (communityRes[2] == 1) {
+          //Append to spaceTracker
+          spaceTracker.push_back(token);
+          break;
+        }
+
+      }
+
+      //Draw a Chance card
+      if (token == 8 || token == 23 || token == 37){
+        if (topChance == 16){
+          topChance -= 16;
+        }
+
+        chanceTrack.push_back(chance[topChance]);
+
+        chanceRes = chanceDraw(
+          token,
+          chance[topChance],
+                count
+        );
+
+        token = chanceRes[0];
+        count = chanceRes[1];
+
+        topChance += 1;
+
+        if (chanceRes[2] == 1){
+          //Append to spaceTracker
+          spaceTracker.push_back(token);
+          break;
+        }
+      }
     }
   }
 
 
+  int doubles = sum(dTracker);
+
   return List::create(
-    _["chance"]     = chance,
+    // _["doubles"]    = doubles,
+    // _["dTracker"]   = dTracker,
     _["space"]      = spaceTracker,
-    _["utilities"]  = utility,
-    _["railroads"]  = railroads,
-    _["count"]      = count
+    // _["turns"]      = countTrack,
+    // _["count"]      = count,
+    _["Community"]  = communityTrack
   );
 }
 
