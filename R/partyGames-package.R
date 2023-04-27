1bcc7e55dff2362e0924b024d2f0b5f4516c3454
Kline DuBose <kline.dubose@hotmail.com>
2023-04-26 20:56:50 -0600

    simulation of monopoly function

--- R/partyGames-package.R
+++ R/partyGames-package.R
@@ -2,4 +2,7 @@
 #' @importFrom parallel makeCluster parSapply stopCluster
 #' @importFrom graphics barplot
 #' @useDynLib partyGames, .registration = TRUE
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
 NULL
--- src/simMonopoly.cpp
+++ src/simMonopoly.cpp
@@ -4,26 +4,26 @@ using namespace Rcpp;
 
 // [[Rcpp::plugins(openmp)]]
 
+
 List monopoly(int maxTurns, int sides, int numDice);
 
 // [[Rcpp::export]]
 List simMonopoly(
-  int numGames,
-  int maxTurns,
-  int sides,
-  int numDice,
-  int cores = 1
+    int numGames,
+    int maxTurns,
+    int sides,
+    int numDice,
+    int cores = 1
 ) {
-  List results;
-  List finalResults;
+  List results(numGames);
+  // List finalResults;
 
   // setting the cores
   omp_set_num_threads(cores);
 
-#pragma omp parallel for private(numGames, maxTurns, sides, numDice, i) default(none)
-  for (int i = 0; i < numGames; ++i){
-    results = monopoly(maxTurns, sides, numDice);
-    finalResults.push_back(results);
+#pragma omp parallel for shared(numGames, maxTurns, sides, numDice, results) default(none)
+  for (int i = 0; i < numGames; i++){
+    results[i] = monopoly(maxTurns, sides, numDice);
   }
 
   return results;
