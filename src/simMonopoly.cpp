720cb2e0d52056ec20b17cd742d37666764ce4c6
Kline DuBose <kline.dubose@hotmail.com>
2023-04-29 00:10:57 -0600

    Attempted to fix simMonopoly. Trouble with the parallel loop

--- src/simMonopoly.cpp
+++ src/simMonopoly.cpp
@@ -13,29 +13,57 @@ using namespace Rcpp;
 arma::mat monopoly(int maxTurns, int sides, int numDice);
 
 // [[Rcpp::export]]
-arma::mat simMonopoly(
+std::vector< std::vector< int > > simMonopoly(
     int numGames = 10,
     int maxTurns = 500,
     int sides = 6,
     int numDice = 2,
     int cores = 1
 ) {
-  arma::mat results(numGames, 40);
+  std::vector < std::vector< int > > results(numGames, std::vector<int>(40));
 
 #ifdef _OPENMP
   // setting the cores
   omp_set_num_threads(cores);
-#else
-  omp_set_num_threads(1);
 #endif
 
-#pragma omp parallel for shared(numGames, maxTurns, sides, numDice, results) default(none)
-  for (int i = 0; i < numGames; i++){
-    arma::mat game = monopoly(maxTurns, sides, numDice);
+#ifdef _OPENMP
+  int effective_ncores = cores;
+#else
+  int effective_ncores = 1;
+#endif
 
-    results.row(i) = game.row(1);
+  std::vector< int > nreplicates(effective_ncores, 0);
+  std::vector< int > nreplicates_csum(effective_ncores, 0);
+  int sums = 0u;
+  for (int i = 0; i < effective_ncores; i++){
+    nreplicates[i] = static_cast<int>(std::floor(numGames/effective_ncores));
 
-}
+    nreplicates_csum[i] = sums;
+
+    sums += nreplicates[i];
+  }
+
+  if (sums < numGames) nreplicates[effective_ncores - 1] += (numGames - sums);
+
+#pragma omp parallel for shared(numGames, nreplicates, effective_ncores, maxTurns, sides, numDice, results) default(none)
+{
+    auto thd = omp_get_thread_num();
+    for (int i = 0; i < nreplicates[thd]; i++){
+      arma::mat game = monopoly(maxTurns, sides, numDice);
+
+      arma::rowvec gamerow = game.row(1);
+
+      std::vector<int> vec(gamerow.begin(), gamerow.end());
+
+      for (int j = 0; j < 40; j++){
+
+        results[i][j] += vec[j];
+      }
+    }
+ }
+
+
   return results;
 
 }
